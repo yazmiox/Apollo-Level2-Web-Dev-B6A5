3d3ddaebb5f34c0a8b2ee754a9bce254ec430853
Yasin Ahmed <myselfyasinahmed@gmail.com>
2026-03-30 02:32:57 +0600

    fix: add R2 hostname in remotePatterns in nextjs config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,7 +6,13 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: process.env.R2_HOSTNAME!,
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
