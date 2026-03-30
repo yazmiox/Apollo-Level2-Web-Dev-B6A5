60e37f01d77bac093c0bf2ee40f5222cdb75bef8
Yasin Ahmed <myselfyasinahmed@gmail.com>
2026-03-31 00:16:02 +0600

    fix: add server actions for equpments that were missing

--- frontend/app/actions/category.ts
+++ frontend/app/actions/category.ts
@@ -1,8 +1,48 @@
 "use server"
 
+import slugify from "slugify";
 import httpClient from "../lib/httpClient";
+
+type CreateCategoryInput = {
+    name: string;
+    description: string
+}
+
+export const createCategory = async (data: CreateCategoryInput) => {
+    const slug = slugify(data.name, {
+        lower: true,
+        strict: true
+    });
+    const response = await httpClient.post("/categories", {
+        ...data,
+        slug
+    });
+    return response;
+}
 
 export const getAllCategories = async () => {
     const response = await httpClient.get("/categories");
+    return response;
+}
+
+export const getCategory = async (slug: string) => {
+    const response = await httpClient.get(`/categories/${slug}`);
+    return response;
+}
+
+export const updateCategory = async (id: string, data: CreateCategoryInput) => {
+    const slug = slugify(data.name, {
+        lower: true,
+        strict: true
+    });
+    const response = await httpClient.patch(`/categories/${id}`, {
+        ...data,
+        slug
+    });
+    return response;
+}
+
+export const deleteCategory = async (id: string) => {
+    const response = await httpClient.delete(`/categories/${id}`);
     return response;
 }
