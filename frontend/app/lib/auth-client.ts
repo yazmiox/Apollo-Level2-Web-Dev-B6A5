bda06d65dcd442bea7c0db65ef27b2b6bcc598c9
Yasin Ahmed <myselfyasinahmed@gmail.com>
2026-04-11 15:39:49 +0600

    fix: test push

--- frontend/app/lib/auth-client.ts
+++ frontend/app/lib/auth-client.ts
@@ -1,3 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 
-export const authClient = createAuthClient()
+export const authClient = createAuthClient({
+    baseURL: process.env.NEXT_PUBLIC_SERVER_URL
+})
